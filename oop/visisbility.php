aea249bd4ba7c08702781beccbd0c5b1148531f8
MuhamadRizky14 <203040043@mail.unpas.ac.id>
2022-10-03 20:41:54 +0700

    Menyelesaikan video 9

--- oop/visisbility.php
+++ oop/visisbility.php
@@ -3,14 +3,21 @@
 class Produk {
     public $judul, 
            $penulis,
-           $penerbit,
-           $harga;
+           $penerbit;
+
+    protected $diskon = 0;
+
+    private $harga;
 
     public function __construct( $judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0 ) {
         $this->judul = $judul;
         $this->penulis = $penulis;
         $this->penerbit = $penerbit;
         $this->harga = $harga;
+    }
+
+    public function getHarga() {
+        return $this->harga - ( $this->harga * $this->diskon / 100 );
     }
 
     public function getLabel() {
@@ -52,6 +59,10 @@ class Game extends Produk {
         $this->waktuMain = $waktuMain;
     }
 
+    public function setDiskon( $diskon ) {
+        $this->diskon = $diskon;
+    }
+
     public function getInfoProduk() {
         $str = "Game : " . parent::getInfoProduk() . " ~ {$this->waktuMain} Jam.";
         return $str;
@@ -73,3 +84,7 @@ $produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
 echo $produk1->getInfoProduk();
 echo "<br>";
 echo $produk2->getInfoProduk();
+echo "<hr>";
+
+$produk2->setDiskon(50);
+echo $produk2->getHarga();
