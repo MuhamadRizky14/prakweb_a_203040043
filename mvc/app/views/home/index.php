38bb98941da3ed76da73719d54c84debf8b7706c
MuhamadRizky14 <203040043@mail.unpas.ac.id>
2022-10-09 02:40:15 +0700

    Menyelesaikan video 6 assets

--- mvc/app/views/home/index.php
+++ mvc/app/views/home/index.php
@@ -1,12 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" href="">
-  <title>Halaman Home</title>
-</head>
-<body>
-  <h1>Selamat Datang di Website Saya</h1>
-</body>
-</html>
+<div class="container">
+  <div class="jumbotron mt-4">
+    <h1 class="display-4">Selamat Datang di Website Saya!</h1>
+    <p class="lead">Buat kamu yang disana</p>
+    <hr class="my-4">
+    <p>Jaga kesehatan ya!!!
+        dan jangan lupa makan oke :)
+    </p>
+    <a class="btn btn-primary btn-lg" href="" role="button">Sehat dan Bahagia Selalu!</a>
+  </div>
+</div>
